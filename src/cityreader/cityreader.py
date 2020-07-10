8de042f89371d615596316162d5292864883a02b
Genard Ivan Tejano <genardivanteajno@Genards-MacBook-Pro.local>
2020-07-10 09:56:26 -0700

    can now read every city in correct format

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,7 +1,16 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
 
+import csv 
 
+class City:
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat 
+    self.lon
+
+  def __str__(self):
+    return f"{self.name}: ({self.lat}, {self.lon})"
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
 #
@@ -20,8 +29,13 @@ def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
-    return cities
+    with open('cities.csv', 'r') as csv_file:
+      csv_reader = csv.reader(csv_file)
+      next(csv_reader)
+
+      for line in csv_reader:
+        print(f"{line[0]}, {line[3], line [4]}")
+      return cities
 
 cityreader(cities)
 
